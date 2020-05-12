0d20380924d280189ed16bf19656eb166f0154e5
Evaldas Alčauskis <evaldas.alcauskis@gmail.com>
2020-05-12 11:20:27 +0300

    Migrate from REST wrapping to appending logic

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,44 +1,46 @@
+const companyRepository = require('../repositories/companyRepository')
+const employeeRepository = require('../repositories/employeeRepository')
+
 const resolvers = {
   Query: {
-    companies: (_source, _args, { dataSources }) => {
-      return dataSources.MigrationApi.getCompanies();
+    companies: (_source, _args) => {
+      return companyRepository.getAll();
     },
-    company: (_source, { id }, { dataSources }) => {
-      return dataSources.MigrationApi.getCompany(id);
+    company: (_source, { id }) => {
+      return companyRepository.getById(id);
     },
-    employees: (_source, { companyId }, { dataSources }) => {
-      return dataSources.MigrationApi.getCompanyEmployees(companyId);
+    employees: (_source, { companyId }) => {
+      return employeeRepository.getAllByCompanyId(companyId);
     },
-    employee: (_source, { id }, { dataSources }) => {
-      return dataSources.MigrationApi.getEmployee(id);
+    employee: (_source, { id }) => {
+      return employeeRepository.getById(id);
     },
   },
   Mutation: {
-    createCompany: (_source, { title, address }, { dataSources }) => {
-      return dataSources.MigrationApi.createCompany(title, address);
+    createCompany: (_source, { title, address }) => {
+      return companyRepository.save({title, address});
     },
-    deleteCompany: (_source, { id }, { dataSources }) => {
-      return dataSources.MigrationApi.deleteCompany(id);
+    deleteCompany: (_source, { id }) => {
+      return companyRepository.deleteById(id);
     },
     createEmployee: (
       _source,
-      { companyId, name, jobName },
-      { dataSources }
+      { companyId, name, jobName }
     ) => {
-      return dataSources.MigrationApi.createEmployee(companyId, name, jobName);
+      return employeeRepository.save({companyId, name, jobName});
     },
-    deleteEmployee: (_source, { id }, { dataSources }) => {
-      return dataSources.MigrationApi.deleteEmployee(id);
+    deleteEmployee: (_source, { id }) => {
+      return employeeRepository.deleteById(id);
     },
   },
   Employee: {
-    company: ({ companyId }, _, { dataSources }) => {
-      return dataSources.MigrationApi.getCompany(companyId);
+    company: ({ companyId }, _) => {
+      return companyRepository.getById(companyId);
     },
   },
   Company: {
-    employees: ({ id }, _, { dataSources }) => {
-      return dataSources.MigrationApi.getCompanyEmployees(id);
+    employees: ({ id }, _) => {
+      return employeeRepository.getAllByCompanyId(id);
     },
   },
 };
